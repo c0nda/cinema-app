b87bb877af04e160b90fc4bad0e0fab8d8f4cfc6
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2023-08-04 14:39:10 +0400

    Added new Response

---
+++ app/src/main/java/com/android/cinemaapp/data/remote/retrofit/response/SearchResponse.kt
@@ -0,0 +1,12 @@
+package com.android.cinemaapp.data.remote.retrofit.response
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class SearchResponse(
+    @SerialName("page") val page: Int,
+    @SerialName("results") val results: List<ResultResponse>,
+    @SerialName("total_pages") val totalPages: Int,
+    @SerialName("total_results") val totalResults: Int
+)
