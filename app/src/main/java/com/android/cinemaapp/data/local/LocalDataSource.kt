85c6911c31ec36b29f64b49e25ecd085961245ab
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2023-08-06 13:24:19 +0400

    MovieOld is replaced to Movie. Added functions insertMovies and insertMovieDetails

--- app/src/main/java/com/android/cinemaapp/data/local/LocalDataSource.kt
+++ app/src/main/java/com/android/cinemaapp/data/local/LocalDataSource.kt
@@ -1,10 +1,15 @@
 package com.android.cinemaapp.data.local
 
-import com.android.cinemaapp.model.MovieOld
+import com.android.cinemaapp.model.Movie
+import com.android.cinemaapp.model.MovieDetails
 
 interface LocalDataSource {
 
-    suspend fun loadMovies(): List<MovieOld>
+    suspend fun loadMovies(): List<Movie>
 
-    suspend fun loadMovie(movieId: Int): MovieOld?
+    suspend fun loadMovie(movieId: Int): Movie
+
+    fun insertMovies(movies: List<Movie>)
+
+    fun insertMovieDetails(details: MovieDetails)
 }
