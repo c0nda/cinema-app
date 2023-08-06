f1bee18d77bffa5db2c30500bd6e8a2a35114d17
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2023-08-06 16:43:56 +0400

    Fixed mistakes with using Relation in Entity

--- app/src/main/java/com/android/cinemaapp/data/local/room/entities/MovieDetailsDB.kt
+++ app/src/main/java/com/android/cinemaapp/data/local/room/entities/MovieDetailsDB.kt
@@ -1,22 +1,26 @@
 package com.android.cinemaapp.data.local.room.entities
 
 import androidx.room.Entity
-import androidx.room.Relation
+import androidx.room.ForeignKey
 
-@Entity(tableName = "MovieDetails")
+@Entity(tableName = "MovieDetails",
+    primaryKeys = ["parentId"],
+    foreignKeys = [ForeignKey(
+        entity = MovieDB::class,
+        parentColumns = ["id"],
+        childColumns = ["parentId"],
+        onDelete = ForeignKey.CASCADE
+    )]
+)
 data class MovieDetailsDB(
     val id: Int,
     val pgAge: Int,
     val title: String,
-    @Relation(parentColumn = "parentId", entityColumn = "parentId")
-    val genres: List<GenreDB>,
     val reviewCount: Int,
     val isLiked: Boolean,
     val rating: Int,
     val runtime: Int,
     val detailImageUrl: String,
     val storyLine: String,
-    @Relation(parentColumn = "parentId", entityColumn = "parentId")
-    val actors: List<ActorDB>,
     val parentId: Int
 )
