d026b88cc33f44f19333c6cd3b2c9b0ca69ed1b2
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2023-08-06 16:43:26 +0400

    Changed returned type

--- app/src/main/java/com/android/cinemaapp/data/local/room/dao/MovieDetailsDao.kt
+++ app/src/main/java/com/android/cinemaapp/data/local/room/dao/MovieDetailsDao.kt
@@ -5,12 +5,13 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.android.cinemaapp.data.local.room.entities.MovieDetailsDB
+import com.android.cinemaapp.data.local.room.entities.MovieDetailsWithActorsAndGenres
 
 @Dao
 interface MovieDetailsDao {
 
     @Query("SELECT * FROM MovieDetails")
-    fun getMovieDetails(): MovieDetailsDB
+    fun getMovieDetails(): MovieDetailsWithActorsAndGenres
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertMovieDetails(details: MovieDetailsDB)
