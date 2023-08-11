9edeadc2e61eb5ee552be6125cb9fd69dc41979a
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2023-08-11 20:05:50 +0400

    Removed relation with foreign key

--- app/src/main/java/com/android/cinemaapp/data/local/room/entities/GenreDB.kt
+++ app/src/main/java/com/android/cinemaapp/data/local/room/entities/GenreDB.kt
@@ -1,21 +1,12 @@
 package com.android.cinemaapp.data.local.room.entities
 
 import androidx.room.Entity
-import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
 
 
-@Entity(
-    tableName = "genre_id",
-    primaryKeys = ["id"],
-    foreignKeys = [ForeignKey(
-        entity = MovieDB::class,
-        parentColumns = ["id"],
-        childColumns = ["parentId"],
-        onDelete = ForeignKey.CASCADE
-    )]
-)
+@Entity(tableName = "Genre")
 data class GenreDB(
-    val id: Int,
-    val name: String,
-    var parentId: Int
+    @PrimaryKey
+    val genreId: Int,
+    val name: String
 )
