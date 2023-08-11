6a3b5e0d25d17143dcb9febea502914d2bf8a3f8
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2023-08-11 20:10:25 +0400

    Functions loadMovie and loadMovies are changed

--- app/src/main/java/com/android/cinemaapp/repository/MovieRepositoryImpl.kt
+++ app/src/main/java/com/android/cinemaapp/repository/MovieRepositoryImpl.kt
@@ -4,21 +4,41 @@ import com.android.cinemaapp.data.local.LocalDataSource
 import com.android.cinemaapp.data.remote.RemoteDataSource
 import com.android.cinemaapp.model.Movie
 import com.android.cinemaapp.model.MovieDetails
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class MovieRepositoryImpl(
     private val localDataSource: LocalDataSource,
     private val remoteDataSource: RemoteDataSource
 ) : MovieRepository {
 
-    override suspend fun loadMovies(): List<Movie> {
-        return remoteDataSource.loadMovies()
-    }
-
     override suspend fun searchMovies(name: String): List<Movie> {
         return remoteDataSource.searchMovies(name)
     }
 
     override suspend fun loadMovie(movieId: Int): MovieDetails {
-        return remoteDataSource.loadMovie(movieId)
+        return withContext(Dispatchers.IO) {
+            val movieDB = localDataSource.loadMovie(movieId)
+            if (movieDB == null) {
+                val movieFromNetwork = remoteDataSource.loadMovie(movieId)
+                localDataSource.insertMovieDetails(movieFromNetwork)
+                movieFromNetwork
+            } else {
+                movieDB
+            }
+        }
+    }
+
+    override suspend fun loadMovies(): List<Movie> {
+        return withContext(Dispatchers.IO) {
+            val moviesDB = localDataSource.loadMovies()
+            if (moviesDB.isEmpty()) {
+                val moviesFromNetwork = remoteDataSource.loadMovies()
+                localDataSource.insertMovies(moviesFromNetwork)
+                moviesFromNetwork
+            } else {
+                moviesDB
+            }
+        }
     }
 }
