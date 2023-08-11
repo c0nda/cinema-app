8bde4007c10446cf2def61bb32d48e523e53e898
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2023-08-11 20:08:35 +0400

    Associations added to relations

--- app/src/main/java/com/android/cinemaapp/data/local/room/entities/MovieDetailsWithActorsAndGenres.kt
+++ app/src/main/java/com/android/cinemaapp/data/local/room/entities/MovieDetailsWithActorsAndGenres.kt
@@ -1,13 +1,22 @@
 package com.android.cinemaapp.data.local.room.entities
 
 import androidx.room.Embedded
+import androidx.room.Junction
 import androidx.room.Relation
 
-class MovieDetailsWithActorsAndGenres(
+data class MovieDetailsWithActorsAndGenres(
     @Embedded
     val details: MovieDetailsDB,
-    @Relation(parentColumn = "parentId", entityColumn = "parentId")
+    @Relation(
+        parentColumn = "movieDetailsId",
+        entityColumn = "genreId",
+        associateBy = Junction(MovieDetailsGenreCrossRef::class)
+    )
     val genres: List<GenreDB>,
-    @Relation(parentColumn = "parentId", entityColumn = "parentId")
+    @Relation(
+        parentColumn = "movieDetailsId",
+        entityColumn = "actorId",
+        associateBy = Junction(MovieDetailsActorCrossRef::class)
+    )
     val actors: List<ActorDB>
 )
