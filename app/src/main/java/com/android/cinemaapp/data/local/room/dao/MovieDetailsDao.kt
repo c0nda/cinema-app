a799b8c1f44f368768b858b3937ec552d2945998
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2023-08-13 18:06:25 +0400

    Changed list of input arguments of insertMovieDetails function

--- app/src/main/java/com/android/cinemaapp/data/local/room/dao/MovieDetailsDao.kt
+++ app/src/main/java/com/android/cinemaapp/data/local/room/dao/MovieDetailsDao.kt
@@ -5,8 +5,6 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Transaction
-import com.android.cinemaapp.data.local.room.entities.ActorDB
-import com.android.cinemaapp.data.local.room.entities.GenreDB
 import com.android.cinemaapp.data.local.room.entities.MovieDetailsDB
 import com.android.cinemaapp.data.local.room.entities.MovieDetailsWithActorsAndGenres
 
@@ -18,9 +16,5 @@ interface MovieDetailsDao {
     fun getMovieDetails(movieId: Int): MovieDetailsWithActorsAndGenres?
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertMovieDetails(
-        details: MovieDetailsDB,
-        genres: List<GenreDB>,
-        actors: List<ActorDB>
-    )
+    fun insertMovieDetails(details: MovieDetailsDB)
 }
